b30fa8f8c9e39d3dbb85fe8026742cd5be008470
peerless2012 <peerless2012@126.com>
2022-06-26 17:27:37 +0800

    Init Android Project.

---
+++ app/src/main/java/com/peerless2012/sdl/spine/MainActivity.java
@@ -0,0 +1,14 @@
+package com.peerless2012.sdl.spine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
